8f44a06700ac5c8fa1e12e9f93ae9fd9d2790bce
Даниил Понкратенко <ponkratenko.danil@mail.ru>
2022-11-24 22:48:44 +0300

    Очистка сцены, от уже не нужных врагов

--- Assets/Scripts/Spawners/EnemySpawner.cs
+++ Assets/Scripts/Spawners/EnemySpawner.cs
@@ -2,15 +2,18 @@
 using System.Collections;
 using NTC.Global.Pool;
 using UnityEngine;
+using UnityEngine.Serialization;
 
-public class SpawnerPointsV2 : MonoBehaviour
+public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject[] enemy; // массив типов врагов
-    [NonSerialized] private int countSpawnEnemies = 1;
-    public int maxCountSpawnEnemies = 20;
+    [FormerlySerializedAs("enemy")] [SerializeField]
+    private GameObject[] enemies; // массив типов врагов
 
+    private int countSpawnEnemies = 1;
+    private int maxCountSpawnEnemies = 15;
+
+    [SerializeField] private Transform container;
     private Vector3 spawnPoint; // точка спавна
-    [SerializeField]private Transform container;
     private GameObject curEnemy; //Текущий враг
     private GameObject parentPoint; // родительский объект, используется для позиционирования точек
     private GameObject targetAroundRotate; //Цель, вокруг которого будет произведено вращение
@@ -28,21 +31,27 @@ public class SpawnerPointsV2 : MonoBehaviour
 
     [SerializeField] private int minuteCountToAvoidPreviousEnemy;
     [SerializeField] private int countOfEnemiesForRandom;
-    private int countOfAvoidEnemies;
+    [SerializeField] private int countOfAvoidEnemies;
     private float zAngleEnemy;
 
     public TimeManager timeManagerScr;
 
-    private const float ScaleTimeArithmetic = -0.05f;
-    private const float ScaleTimeGeometric = 0.9f;
+    private const float DecreaseTimeArithmetic = -0.05f;
+    private const float DecreaseTimeGeometric = 0.9f;
 
-    public TypeOfTimeProgress typeOfTimeProgress;
+    [FormerlySerializedAs("typeOfTimeProgress")]
+    public TypeOfTimeDecrease typeOfTimeDecrease;
 
-    public enum TypeOfTimeProgress
+    public enum TypeOfTimeDecrease
     {
         Arithmetic,
         Geometric
     };
+
+    private int poolDestroyIndex;
+    [SerializeField] private float startTimeToDestroyPool;
+    [SerializeField] private float timeToDestroyPool;
+    private const int SecondsInMinute = 60;
 
     private void Start()
 
@@ -50,8 +59,9 @@ public class SpawnerPointsV2 : MonoBehaviour
         startPositionY = transform.localPosition.y; //присвоение стартовой позиции по Y (transform.position)
         v3Start = new Vector3(0, startPositionY, 0); // стартовая позиция (Все координаты)
         targetAroundRotate = Player.playerGameObject;
-        timeBtwSpawns = startTimeBtwSpawns; //ставим таймер в ноль, чтобы он заупстился
-        StartCoroutine(StartTimerEnemy()); //заупскаем таймер первый раз(спусковой, дальше он сам себя будет вызывать)
+
+        StartCoroutine(EnemySpawnTimer()); //заупскаем таймер первый раз(спусковой, дальше он сам себя будет вызывать)
+        StartCoroutine(DestroyPoolTimer());
     }
 
     private void IncreaseCountSpawnEnemies()
@@ -62,15 +72,15 @@ public class SpawnerPointsV2 : MonoBehaviour
         }
     }
 
-    private void CheckIfTimeScale()
+    private void CheckTimeToSpawnDecrease()
     {
         if (timeManagerScr.minuteCounter == timeManagerScr.gameTime.Minute) return;
-        switch (typeOfTimeProgress)
+        switch (typeOfTimeDecrease)
         {
-            case TypeOfTimeProgress.Arithmetic:
-                if (startTimeBtwSpawns > ScaleTimeArithmetic)
+            case TypeOfTimeDecrease.Arithmetic:
+                if (startTimeBtwSpawns > DecreaseTimeArithmetic)
                 {
-                    startTimeBtwSpawns += ScaleTimeArithmetic;
+                    startTimeBtwSpawns += DecreaseTimeArithmetic;
                 }
                 else
                 {
@@ -79,10 +89,10 @@ public class SpawnerPointsV2 : MonoBehaviour
 
                 break;
 
-            case TypeOfTimeProgress.Geometric:
+            case TypeOfTimeDecrease.Geometric:
                 if (startTimeBtwSpawns > 0.1f)
                 {
-                    startTimeBtwSpawns *= ScaleTimeGeometric;
+                    startTimeBtwSpawns *= DecreaseTimeGeometric;
                 }
                 else
                 {
@@ -91,7 +101,7 @@ public class SpawnerPointsV2 : MonoBehaviour
 
                 break;
             default:
-                print("Не выставлено значение прогрессии времени спавна");
+                Debug.LogError("Не выставлено значение прогрессии времени спавна");
                 throw new ArgumentOutOfRangeException();
         }
 
@@ -109,17 +119,17 @@ public class SpawnerPointsV2 : MonoBehaviour
             index = countOfAvoidEnemies;
         }
 
-        if (index >= enemy.Length)
+        if (index >= enemies.Length)
         {
-            index = enemy.Length - 1;
+            index = enemies.Length - 1;
         }
 
-        return enemy[index];
+        return enemies[index];
     }
 
     // ReSharper disable Unity.PerformanceAnalysis
     // ReSharper disable once FunctionRecursiveOnAllPaths
-    private IEnumerator StartTimerEnemy() //корутина таймера
+    private IEnumerator EnemySpawnTimer() //корутина таймера
     {
         timeBtwSpawns = startTimeBtwSpawns; //обнуляем таймер создания крипа
         while (timeBtwSpawns > 0) //если время больше 0, то уменьшаем его по чуть-чуть
@@ -134,21 +144,21 @@ public class SpawnerPointsV2 : MonoBehaviour
         }
 
         CheckPointReset();
-        CheckIfTimeScale();
+        CheckTimeToSpawnDecrease();
 
-        StartCoroutine(StartTimerEnemy()); //перезапуск таймера спавна
+        StartCoroutine(EnemySpawnTimer()); //перезапуск таймера спавна
     }
 
     private void SpawnEnemies()
     {
         spawnPoint = gameObject.transform.position;
         zAngleEnemy = random.Range(0f, 350f); // рандомный добавочный угол через быстрый рандом для поворота врага
-        
+
         //curEnemy = NightPool.Spawn(GetRandomEnemyType(), spawnPoint);
         curEnemy = NightPool.Spawn(GetRandomEnemyType(), spawnPoint, Quaternion.Euler(0f, 0f, zAngleEnemy));
 
         curEnemy.transform.SetParent(container);
-        
+
         RotatePoint();
     }
 
@@ -167,5 +177,33 @@ public class SpawnerPointsV2 : MonoBehaviour
         if (resetCounter != PointsResetLimit) return; //проверка счетчика
         transform.localPosition = v3Start;
         resetCounter %= PointsResetLimit; //обнуление счетчика
+    }
+
+    private IEnumerator DestroyPoolTimer()
+    {
+        startTimeToDestroyPool = (minuteCountToAvoidPreviousEnemy + 2) * SecondsInMinute;
+        timeToDestroyPool = startTimeToDestroyPool; //обнуляем таймер 
+
+        while (timeToDestroyPool > 0) //если время больше 0, то уменьшаем его по чуть-чуть
+        {
+            timeToDestroyPool -= Time.deltaTime; //само уменьшение таймера
+            yield return null; //продолжить выполнение после этого кадра
+        }
+
+        DestroyEnemyPool();
+    }
+
+    private void DestroyEnemyPool()
+    {
+        if (poolDestroyIndex == enemies.Length) return;
+        
+        print(poolDestroyIndex);
+        var gameObjectOfPoolBeingDestroyed = NightPool.Spawn(enemies[poolDestroyIndex]);
+        NightPool.DestroyPool(gameObjectOfPoolBeingDestroyed);
+        Destroy(gameObjectOfPoolBeingDestroyed);
+        poolDestroyIndex++;
+
+        if (poolDestroyIndex >= enemies.Length) return;
+        StartCoroutine(DestroyPoolTimer());
     }
 }

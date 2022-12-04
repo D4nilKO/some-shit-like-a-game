c3c18af6e07d21d1ecdb9d670cfc639f7e69e290
Даниил Понкратенко <ponkratenko.danil@mail.ru>
2022-12-04 16:06:18 +0300

    Перенос движения врага в отдельный скрипт

---
+++ Assets/Components/Enemies/MoveToPlayer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Components.Enemies
+{
+    public class MoveToPlayer: MonoBehaviour
+    {
+        [SerializeField] private float speed;
+        [SerializeField] private bool stalking = true;
+
+        public void StalkingMove()
+        {
+            transform.position = Vector2.MoveTowards(gameObject.transform.position, Player.playerTransform.position,
+                speed * Time.fixedDeltaTime);
+        }
+
+        private void FixedUpdate()
+        {
+            if (stalking)
+            {
+                StalkingMove();
+            }
+            else
+            {
+                OnceTurnedMove();
+            }
+        }
+
+        private void OnceTurnedMove()
+        {
+            //повернуться в сторону игрока
+            transform.Translate(Vector3.right * (speed * Time.deltaTime));
+        }
+    }
+}
